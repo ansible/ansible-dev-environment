81a8e9e1322b17f70f8ee29dd037d4968886d301
Bradley A. Thornton <bthornto@redhat.com>
2025-04-08 13:39:13 -0700

    Cleanup logic

--- src/ansible_dev_environment/definitions.py
+++ src/ansible_dev_environment/definitions.py
@@ -43,44 +43,25 @@ class AnsibleCfg:
         The configparser doesn't preserve comments, so we need to read the file
         and write it back with the new collection path.
         """
-        config = ConfigParser()
-        config.read(self.path)
-        if not config.has_section("defaults"):
-            with self.path.open(mode="r+") as file:
-                content_str = file.read()
-                file.seek(0, 0)
-                file.truncate()
-                file.write("[defaults]\ncollections_path = .\n" + content_str)
-                file.write("\n")
-            return
+        contents = self.path.read_text().splitlines()
+        collections_path = "collections_path = ."
 
-        if not config.has_option("defaults", "collections_path"):
-            with self.path.open(mode="r+") as file:
-                content_list = file.read().splitlines()
-                idx = content_list.index("[defaults]") + 1
-                content_list.insert(idx, "collections_path = .")
-                file.seek(0, 0)
-                file.truncate()
-                file.write("\n".join(content_list))
-                file.write("\n")
-            return
+        if "[defaults]" not in contents:
+            contents.insert(0, "[defaults]")
 
-        with self.path.open(mode="r+") as file:
-            content_list = file.read().splitlines()
-            idx = next(
-                i for i, line in enumerate(content_list) if line.startswith("collections_path")
-            )
-            content_list[idx] = "collections_path = ."
-            file.seek(0, 0)
-            file.truncate()
-            file.write("\n".join(content_list))
-            file.write("\n")
-        return
+        idx = [i for i, line in enumerate(contents) if line.startswith("collections_path")]
+
+        if idx:
+            contents[idx[0]] = collections_path
+        else:
+            insert_at = contents.index("[defaults]") + 1
+            contents.insert(insert_at, collections_path)
+
+        with self.path.open(mode="w") as file:
+            file.write("\n".join(contents) + "\n")
 
     def author_new(self) -> None:
         """Author the file and update it."""
-        config = ConfigParser()
-        config.add_section("defaults")
-        config.set("defaults", "collections_path", ".")
-        with self.path.open(mode="w") as f:
-            config.write(f)
+        contents = ["[defaults]", "collections_path = ."]
+        with self.path.open(mode="w") as file:
+            file.write("\n".join(contents) + "\n")
